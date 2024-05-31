87aaeb9ff3f2331a078d5ff6bde2e51834c7934c
KimchiTrader <oksusu.koala@gmail.com>
2024-05-31 09:28:08 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -275,7 +275,7 @@
 			margin:30,
 			nav:false,
 			smartSpeed: 2000,
-			autoplay: 3000,
+			autoplay: 6000,
 			navText: [ '<span class="flaticon-left"></span>', '<span class="flaticon-right"></span>' ],
 			responsive:{
 				0:{
